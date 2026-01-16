6dcd50dc7bbec1122ce3346a7e4085d46e031c0f
Tarun Goyal <targoy@qti.qualcomm.com>
2026-01-16 10:13:17 +0530

    Test new version of CP scanner (#1)

--- acdb/api/acdb_begin_pack.h
+++ acdb/api/acdb_begin_pack.h
@@ -7,7 +7,7 @@
 *      pack ACPH API data structures.
 *
 * \copyright
-*  Copyright (c) Qualcomm Innovation Center, Inc. All rights reserved.
+*  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 *  SPDX-License-Identifier: BSD-3-Clause
 *
 *=============================================================================
