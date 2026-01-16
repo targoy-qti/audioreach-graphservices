60c1af50465fea007c8389a4c77be7f1dd6f7217
Tarun Goyal <targoy@qti.qualcomm.com>
2026-01-16 10:22:58 +0530

    Test CP

--- acdb/api/acdb.h
+++ acdb/api/acdb.h
@@ -9,7 +9,7 @@
 *      Database (ACDB) module.
 *
 * \copyright
-*  Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+*  Copyright (c) 2026 Qualcomm Technologies, Inc. and/or its subsidiaries.
 *  SPDX-License-Identifier: BSD-3-Clause
 *
 *=============================================================================
